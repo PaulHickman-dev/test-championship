9f262877814c36a9d5981f4791cfba8888b16902
Paul Hickman <made@up.com>
2019-09-14 11:53:34 +0100

    Fixed output path not matching repository name. Included dist folder in code to commit so it is published on github pages

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'test-championship-simulator'`, () => {
+  it(`should have as title 'test-championship'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('test-championship-simulator');
+    expect(app.title).toEqual('test-championship');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to test-championship-simulator!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to test-championship!');
   });
 });
